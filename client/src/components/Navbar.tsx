b6189156400dd55ecb7fff71533bb597a0c74470
laycookie <80195126+laycookie@users.noreply.github.com>
2023-01-08 15:46:31 -0800

    Navbar bugfix

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,6 +6,26 @@ type Props = {};
 export default function Navbar({}: Props) {
   const location = useLocation();
   const [dropDown, setDropDown] = React.useState<boolean>(false);
+  const [pageTitle, setPageTitle] = React.useState<string>("");
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        setPageTitle("Home");
+        break;
+      case "/blog":
+        setPageTitle("Blog");
+        break;
+      case "/portfolio":
+        setPageTitle("Portfolio");
+        break;
+      case "/contact":
+        setPageTitle("Contact");
+        break;
+      default:
+        setPageTitle("Error");
+    }
+  }, [location.pathname]);
 
   return (
     <nav
@@ -21,61 +41,48 @@ export default function Navbar({}: Props) {
             Home
           </Link>
           <Link to={location.pathname} className="nav-btn md:hidden">
-            {document.title}
+            {pageTitle}
           </Link>
         </li>
         <li>
           <ul className="flex space-x-4">
-
-            <li>
-              <Link to="/blog" className="nav-btn nav-hide">
-                Blog
-              </Link>
+            <li className="nav-btn nav-hide">
+              <Link to="/blog">Blog</Link>
             </li>
-            <li>
-              <Link to="/portfolio" className="nav-btn nav-hide">
-                Portfolio
-              </Link>
+            <li className="nav-btn nav-hide">
+              <Link to="/portfolio">Portfolio</Link>
             </li>
-            <li>
-              <Link to="/contact" className="nav-btn nav-hide">
-                Contact
-              </Link>
+            <li className="nav-btn nav-hide">
+              <Link to="/contact">Contact</Link>
             </li>
-            <li>
-              <button className="md:hidden" onClick={() => {
-                setDropDown(!dropDown)
-                }}>
-X
+            <li className="">
+              <button
+                className="md:hidden"
+                onClick={() => {
+                  setDropDown(!dropDown);
+                }}
+              >
+                X
               </button>
             </li>
           </ul>
         </li>
       </ul>
-      
-      <ul className={dropDown ? "grid justify-center" : "hidden"}>
-        <li>
-              <Link to="/" className="nav-btn-minimized">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/blog" className="nav-btn-minimized">
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link to="/portfolio" className="nav-btn-minimized">
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" className="nav-btn-minimized">
-                Contact
-              </Link>
-            </li>
-        </ul>
-     
+
+      <ul className={dropDown ? "grid justify-center md:hidden" : "hidden"}>
+        <li className="nav-btn-minimized">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-btn-minimized">
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li className="nav-btn-minimized">
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li className="nav-btn-minimized">
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
