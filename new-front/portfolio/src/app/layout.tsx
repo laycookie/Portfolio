997b3ae017f924a694ce153d6e6963c8f026a9e8
laycookie <80195126+laycookie@users.noreply.github.com>
2023-02-22 20:09:51 -0800

    dark mode implemintation
    - doesn't immediately start on page load

--- new-front/portfolio/src/app/layout.tsx
+++ new-front/portfolio/src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,9 +8,28 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [isDark, setIsDark] = useState(true);
+  const [isDark, setIsDark] = useState<boolean | null>(null);
+  // sets the theme to dark or light
+  useLayoutEffect(() => {
+    const isDarkL = localStorage.getItem("isDark");
+    if (isDarkL === "true") {
+      setIsDark(true);
+    } else if (isDarkL === "false") {
+      setIsDark(false);
+    } else if (isDarkL === null) {
+      // check if the user has os set to dark mode
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        setIsDark(true);
+        localStorage.setItem("isDark", "true");
+      } else {
+        setIsDark(false);
+        localStorage.setItem("isDark", "false");
+      }
+    }
+  }, []);
+
   return (
-    <html lang="en" className={"Defaults " + (isDark ? "dark" : "")}>
+    <html lang="en" className={isDark ? " dark" : ""}>
       <body>{children}</body>
     </html>
   );
--- new-front/portfolio/src/app/page.tsx
+++ new-front/portfolio/src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "components/Navbar";
 
 export default function Home() {
   return (
-    <main>
+    <main className="defaults">
       <Navbar pageTitle="Home" />
       <h1>test</h1>
     </main>
