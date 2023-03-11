8a1d0ef1fe8d297c9909a929e461ce75c18a2d50
laycookie <80195126+laycookie@users.noreply.github.com>
2023-03-11 07:19:46 -0800

    Add responsiveness to grid

--- src/app/SkillGrid.tsx
+++ src/app/SkillGrid.tsx
@@ -30,47 +30,60 @@ export default function SkillGrid({}: Props) {
           className="grid grid-cols-1 lg:grid-cols-2 grid-rows-4 gap-[2px]
            "
         >
-          <h2 className="bg-dark-main flex justify-center items-center">
+          <h2 className="bg-dark-main flex justify-center items-center text-2xl sm:text-3xl font-semibold">
             Familiar technologies
           </h2>
-          <h2 className="bg-dark-main hidden lg:flex justify-center items-center">
+          <h2 className="bg-dark-main hidden lg:flex justify-center items-center text-3xl font-semibold">
             Fun demos
           </h2>
 
-          <div className="flex  bg-dark-main">
-            <h3 className="w-32 my-auto pl-4">Lang.</h3>
-            <div className="pl-32 my-auto">
-              <p>Python</p>
-              <p>Node </p>
-              <p>C</p>
-              <p>C# </p>
-              <p>JavaScript</p>
-              <p>Typescript</p>
+          <div className="bg-dark-main flex flex-col justify-center">
+            <div className="sm:flex sm:py-4 bg-dark-main">
+              <h3 className="sm:w-32 my-auto sm:pl-4 text-xl text-center sm:text-left font-semibold">
+                Lang.
+              </h3>
+              <div className="pl-0 sm:pl-32 my-auto text-center sm:text-left">
+                <p>Python</p>
+                <p>Node </p>
+                <p>C</p>
+                <p>C# </p>
+                <p>JavaScript</p>
+                <p>Typescript</p>
+              </div>
             </div>
           </div>
           <div className="row-span-3 py-4 bg-dark-main hidden lg:block"></div>
-          <div className="bg-dark-main">
-            <div className="flex py-4">
-              <h3 className="w-32 my-auto justify-center pl-4">Front-end</h3>
-              <div className="pl-32 my-auto">
+          <div className="bg-dark-main flex flex-col justify-center py-4">
+            <div className="sm:flex pb-2">
+              <h3 className="sm:w-32 my-auto sm:pl-4 text-xl text-center sm:text-left font-semibold">
+                Front-end
+              </h3>
+              <div className="pl-0 sm:pl-32 my-auto text-center sm:text-left">
                 <p>React</p>
                 <p>Three.js</p>
                 <p>TailwindCSS</p>
               </div>
             </div>
-            <div className="flex py-4">
-              <h3 className="w-32 my-auto pl-4">Back-end</h3>
-              <div className="pl-32 my-auto">
+
+            <div className="sm:flex">
+              <h3 className="sm:w-32 my-auto sm:pl-4 text-xl text-center sm:text-left font-semibold">
+                Back-end
+              </h3>
+              <div className="pl-0 sm:pl-32 my-auto text-center sm:text-left">
                 <p>Express</p>
                 <p>Flask</p>
               </div>
             </div>
           </div>
-          <div className="flex py-4 bg-dark-main">
-            <h3 className="w-32 my-auto pl-4">Misc</h3>
-            <div className="pl-32 my-auto">
-              <p>Prisma</p>
-              <p>SQL</p>
+          <div className="bg-dark-main flex flex-col justify-center">
+            <div className=" sm:flex py-auto sm:py-4 ">
+              <h3 className="sm:w-32 my-auto sm:pl-4 text-xl text-center sm:text-left font-semibold">
+                Misc
+              </h3>
+              <div className="pl-0 sm:pl-32 my-auto text-center sm:text-left">
+                <p>Prisma</p>
+                <p>SQL</p>
+              </div>
             </div>
           </div>
         </div>
