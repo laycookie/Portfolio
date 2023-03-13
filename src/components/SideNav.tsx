4f88624884841f161a0d75e052df4c751b807129
laycookie <80195126+laycookie@users.noreply.github.com>
2023-03-12 21:08:44 -0700

    SideBar Finished

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,13 +1,57 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import "./SideNav.css";
 
-type Props = {};
+type Props = { children: React.ReactNode };
 
-export default function SideNav({}: Props) {
+type SectionPrepNav = { name: string; position: number };
+
+export default function SideNav({ children }: Props) {
+  const [sections, setSections] = React.useState<SectionPrepNav[]>(
+    [] as SectionPrepNav[]
+  );
+  useEffect(() => {
+    const sectionsCli: SectionPrepNav[] = [] as SectionPrepNav[];
+    if (!children) return;
+    for (const child of children.props.children) {
+      if (child.type === "section") {
+        /* Takes position of the section using getBoundingClientRect,
+         * and adds vertical window scroll to negate the effect of user scrolling.
+         */
+        const sectionPosition =
+          (document.getElementById(child.props.id)?.getBoundingClientRect()
+            .y as number) + window.scrollY;
+
+        sectionsCli.push({
+          name: child.props.id,
+          position: sectionPosition,
+        } as SectionPrepNav);
+      }
+    }
+    setSections(sectionsCli);
+  }, []);
+
   return (
-    <nav>
-      <ul>
-        <li>test</li>
+    <>
+      <ul
+        className="fixed right-6
+      flex flex-col h-[100vh] justify-center"
+      >
+        {sections.map((section, index) => (
+          <li key={index} className="mt-2">
+            <button
+              onClick={() => {
+                scrollTo(0, section.position);
+              }}
+              className="side-btn"
+            >
+              {section.name}
+              <div />
+            </button>
+          </li>
+        ))}
       </ul>
-    </nav>
+      {children}
+    </>
   );
 }
