36eea78bec4da0292918ae535a047d3970da5685
laycookie <80195126+laycookie@users.noreply.github.com>
2023-03-13 21:07:31 -0700

    Add sample Edu. section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,8 +44,34 @@ export default function Home() {
           <section id="Skills" className="h-[100vh] flex justify-center">
             <SkillGrid />
           </section>
-          <section id="About me" className="h-[100vh] flex justify-center">
+          <section id="About me" className="h-[100vh] flex">
             <h1>Test</h1>
+          </section>
+          <section id="Edu." className="h-[100vh] flex justify-center">
+            <div className="my-16">
+              <h3 className="text-5xl text-center mb-16 font-bold">
+                My coding related education
+              </h3>
+              <div
+                className="rounded-xl p-8"
+                style={{
+                  boxShadow:
+                    "-4px -4px 4px #2D2A32, 0px 40px 60px -17px #141316, inset -2px -2px 4px rgba(0, 0, 0, 0.25), inset 4px 4px 8px rgba(0, 0, 0, 0.25)",
+                }}
+              >
+                <h4 className="mb-4 text-3xl font-semibold">
+                  CS50X Certificate
+                </h4>
+                <h5 className="mb-2 text-xl">
+                  CS50x is a self-paced online computer science course that
+                  covers a wide range of topics, including algorithms, data
+                  structures, internet technologies, and more.
+                </h5>
+                <p className="text-sm">
+                  Certificate ID: 218d13be-02e7-4a50-81da-355c991e4609
+                </p>
+              </div>
+            </div>
           </section>
         </main>
       </SideNav>
