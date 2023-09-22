b19b9639775bbb606950161b9e4f18897c6e7246
laycookie <80195126+laycookie@users.noreply.github.com>
2023-09-21 21:36:17 -0700

    test deployment10

--- src/app/(defults)/blog/create/EditElement.tsx
+++ src/app/(defults)/blog/create/EditElement.tsx
@@ -1,40 +1,38 @@
-// import "@/styles/blogStyles.css";
-// import {useDrag} from "@use-gesture/react";
-// import React from "react";
-// import {ElementsData} from "@/types/blog.d";
+import "@/styles/blogStyles.css";
+import {useDrag} from "@use-gesture/react";
+import React from "react";
+import {ElementsData} from "@/types/blog.d";
 
 type Props = {
-    // index: bigint;
-    // id: string;
-    // elementsData: ElementsData;
-    // setElementsIndexes: React.Dispatch<React.SetStateAction<ElementsData[]>>;
+    index: bigint;
+    id: string;
+    elementsData: ElementsData;
+    setElementsIndexes: React.Dispatch<React.SetStateAction<ElementsData[]>>;
 };
 
-export default function EditElement({}: Props) {
-    // const [contents, setContents]
-    //     = React.useState<string>(elementsData.content);
-    //
-    // const myAttr = {"index": index, "draggableid": id};
-    // const bind = useDrag(({active, movement}) => {
-    //     if (active) {
-    //         console.log(movement);
-    //     } else {
-    //         console.log("not dragging");
-    //     }
-    // }, {});
-    //
-    // return (
-    //     <div className="flex draggable" {...myAttr} {...bind()}>
-    //         <button>=</button>
-    //         <textarea
-    //             value={contents}
-    //             onChange={(e) => {
-    //                 setContents(e.target.value);
-    //             }}
-    //             className="w-full"
-    //         ></textarea>
-    //     </div>
-    // );
+export default function EditElement({index, id, elementsData, setElementsIndexes}: Props) {
+    const [contents, setContents]
+        = React.useState<string>(elementsData.content);
 
-    return <div></div>
+    const myAttr = {"index": index, "draggableid": id};
+    const bind = useDrag(({active, movement}) => {
+        if (active) {
+            console.log(movement);
+        } else {
+            console.log("not dragging");
+        }
+    }, {});
+
+    return (
+        <div className="flex draggable" {...myAttr} {...bind()}>
+            <button>=</button>
+            <textarea
+                value={contents}
+                onChange={(e) => {
+                    setContents(e.target.value);
+                }}
+                className="w-full"
+            ></textarea>
+        </div>
+    );
 }
