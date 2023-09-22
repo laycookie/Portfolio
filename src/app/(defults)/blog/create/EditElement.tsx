ce7e6b12dfde566c41f2bd447b726d7d5dab6bb6
laycookie <80195126+laycookie@users.noreply.github.com>
2023-09-21 21:27:33 -0700

    test deployment6

--- src/app/(defults)/blog/create/EditElement.tsx
+++ src/app/(defults)/blog/create/EditElement.tsx
@@ -1,7 +1,7 @@
-import "@/styles/blogStyles.css";
-import {useDrag} from "@use-gesture/react";
-import React from "react";
-import {ElementsData} from "@/types/blog.d";
+// import "@/styles/blogStyles.css";
+// import {useDrag} from "@use-gesture/react";
+// import React from "react";
+// import {ElementsData} from "@/types/blog.d";
 
 type Props = {
     index: bigint;
@@ -11,28 +11,30 @@ type Props = {
 };
 
 export default function EditElement({index, id, elementsData, setElementsIndexes}: Props) {
-    const [contents, setContents]
-        = React.useState<string>(elementsData.content);
+    // const [contents, setContents]
+    //     = React.useState<string>(elementsData.content);
+    //
+    // const myAttr = {"index": index, "draggableid": id};
+    // const bind = useDrag(({active, movement}) => {
+    //     if (active) {
+    //         console.log(movement);
+    //     } else {
+    //         console.log("not dragging");
+    //     }
+    // }, {});
+    //
+    // return (
+    //     <div className="flex draggable" {...myAttr} {...bind()}>
+    //         <button>=</button>
+    //         <textarea
+    //             value={contents}
+    //             onChange={(e) => {
+    //                 setContents(e.target.value);
+    //             }}
+    //             className="w-full"
+    //         ></textarea>
+    //     </div>
+    // );
 
-    const myAttr = {"index": index, "draggableid": id};
-    const bind = useDrag(({active, movement}) => {
-        if (active) {
-            console.log(movement);
-        } else {
-            console.log("not dragging");
-        }
-    }, {});
-
-    return (
-        <div className="flex draggable" {...myAttr} {...bind()}>
-            <button>=</button>
-            <textarea
-                value={contents}
-                onChange={(e) => {
-                    setContents(e.target.value);
-                }}
-                className="w-full"
-            ></textarea>
-        </div>
-    );
+    return <div></div>
 }
