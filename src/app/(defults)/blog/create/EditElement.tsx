2b68df6f0f99fd7a007494fb927984ed0a39a091
laycookie <80195126+laycookie@users.noreply.github.com>
2023-09-21 21:11:12 -0700

    test deployment2

--- src/app/(defults)/blog/create/EditElement.tsx
+++ src/app/(defults)/blog/create/EditElement.tsx
@@ -11,28 +11,28 @@ type Props = {
 };
 
 export default function EditElement({index, id, elementsData, setElementsIndexes}: Props) {
-    const [contents, setContents]
-        = React.useState<string>(elementsData.content);
-
-    const myAttr = {"index": index, "draggableid": id};
-    const bind = useDrag(({active, movement}) => {
-        if (active) {
-            console.log(movement);
-        } else {
-            console.log("not dragging");
-        }
-    }, {});
-
-    return (
-        <div className="flex draggable" {...myAttr} {...bind()}>
-            <button>=</button>
-            <textarea
-                value={contents}
-                onChange={(e) => {
-                    setContents(e.target.value);
-                }}
-                className="w-full"
-            ></textarea>
-        </div>
-    );
+    // const [contents, setContents]
+    //     = React.useState<string>(elementsData.content);
+    //
+    // const myAttr = {"index": index, "draggableid": id};
+    // const bind = useDrag(({active, movement}) => {
+    //     if (active) {
+    //         console.log(movement);
+    //     } else {
+    //         console.log("not dragging");
+    //     }
+    // }, {});
+    //
+    // return (
+    //     <div className="flex draggable" {...myAttr} {...bind()}>
+    //         <button>=</button>
+    //         <textarea
+    //             value={contents}
+    //             onChange={(e) => {
+    //                 setContents(e.target.value);
+    //             }}
+    //             className="w-full"
+    //         ></textarea>
+    //     </div>
+    // );
 }
--- src/app/(defults)/blog/page.tsx
+++ src/app/(defults)/blog/page.tsx
@@ -11,21 +11,21 @@ type Props = {
 };
 
 export default async function Page({}: Props) {
-    // const {rows} = await sql`SELECT * FROM blogs`;
+    const {rows} = await sql`SELECT * FROM blogs`;
 
     return (
         <main className="defaults contain">
             <h1 className="mt-32 flex justify-center w-full">Blogs</h1>
             <div className="mt-8">
-                {/*{rows.map((blog, index) => (*/}
-                {/*    <InfoCard link={blog.link} key={index}>*/}
-                {/*        <InfoCard.Title title={blog.title}/>*/}
-                {/*        <InfoCard.Text text={blog.description}/>*/}
-                {/*        <InfoCard.Footer*/}
-                {/*            text={`Time to read: ~${blog.readtimeinmin} min.`}*/}
-                {/*        />*/}
-                {/*    </InfoCard>*/}
-                {/*))}*/}
+                {rows.map((blog, index) => (
+                    <InfoCard link={blog.link} key={index}>
+                        <InfoCard.Title title={blog.title}/>
+                        <InfoCard.Text text={blog.description}/>
+                        <InfoCard.Footer
+                            text={`Time to read: ~${blog.readtimeinmin} min.`}
+                        />
+                    </InfoCard>
+                ))}
             </div>
         </main>
     );
