e86ab5a20e07fcf1744680f424215bd28efbd21f
laycookie <80195126+laycookie@users.noreply.github.com>
2023-09-21 22:02:53 -0700

    test deployment12

--- src/app/(defults)/blog/page.tsx
+++ src/app/(defults)/blog/page.tsx
@@ -1,6 +1,6 @@
 import InfoCard from "@/components/InfoCard/InfoCard";
 import type {Metadata} from "next";
-import {sql} from "@vercel/postgres";
+// import {sql} from "@vercel/postgres";
 
 export const metadata: Metadata = {
     title: "Blog",
@@ -9,22 +9,22 @@ export const metadata: Metadata = {
 type Props = {};
 
 export default async function Page({}: Props) {
-    const {rows} = await sql`SELECT * FROM blogs`;
+    // const {rows} = await sql`SELECT * FROM blogs`;
 
     return (
         <main className="defaults contain">
-            <h1 className="mt-32 flex justify-center w-full">Blogs</h1>
-            <div className="mt-8">
-                {rows.map((blog, index) => (
-                    <InfoCard link={blog.link} key={index}>
-                        <InfoCard.Title title={blog.title}/>
-                        <InfoCard.Text text={blog.description}/>
-                        <InfoCard.Footer
-                            text={`Time to read: ~${blog.readtimeinmin} min.`}
-                        />
-                    </InfoCard>
-                ))}
-            </div>
+            {/*<h1 className="mt-32 flex justify-center w-full">Blogs</h1>*/}
+            {/*<div className="mt-8">*/}
+            {/*    {rows.map((blog, index) => (*/}
+            {/*        <InfoCard link={blog.link} key={index}>*/}
+            {/*            <InfoCard.Title title={blog.title}/>*/}
+            {/*            <InfoCard.Text text={blog.description}/>*/}
+            {/*            <InfoCard.Footer*/}
+            {/*                text={`Time to read: ~${blog.readtimeinmin} min.`}*/}
+            {/*            />*/}
+            {/*        </InfoCard>*/}
+            {/*    ))}*/}
+            {/*</div>*/}
         </main>
     );
 }
